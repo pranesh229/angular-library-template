6c7bfa4bb64a3f2a376ce723c8797090344a2462
Pranesh Hanumanth <praneshhanumanth@Praneshs-MacBook-Pro.local>
2019-08-21 14:52:28 -0400

    Angular library

---
+++ projects/header-lib/src/lib/header-lib.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-header-lib',
+  template: `
+    <p>
+      header-lib works!
+    </p>
+  `,
+  styles: []
+})
+export class HeaderLibComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
